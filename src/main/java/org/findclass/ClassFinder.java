89090efe9a82874263669bdfaf118d4df7432b1a
Naimdjon <naimdjon@gmail.com>
2014-05-11 19:37:33 +0200

    find class test

--- src/main/java/org/findclass/ClassFinder.java
+++ src/main/java/org/findclass/ClassFinder.java
@@ -2,10 +2,16 @@ package org.findclass;
 
 
 import java.io.File;
-import java.util.Collection;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class ClassFinder {
-    final private  File dir;
+    final private File dir;
+    private String pattern;
 
     private ClassFinder(File dir) {
         this.dir = dir;
@@ -15,7 +21,35 @@ public class ClassFinder {
         return new ClassFinder(dir);
     }
 
-    public Collection<File> find() {
-        return null;
+    public Collection<String> find(final String pattern) throws IOException {
+        this.pattern = pattern;
+        result.clear();
+        System.out.println("Classfinder Files.list(dir.toPath())" + Arrays.toString(Files.list(dir.toPath()).toArray()));
+        Files.list(dir.toPath())
+                .filter(p -> p.getFileName()
+                        .toString().endsWith(".jar"))
+                .forEach(this::process);
+
+        return result;
+    }
+    final Set<String> result=new LinkedHashSet<>();
+    private void process(final Path p) {
+        try {
+            File j = p.toFile();
+            JarFile jar = new JarFile(j);
+            final Enumeration<JarEntry> e = jar.entries();
+            while (e.hasMoreElements()) {
+                JarEntry entry = e.nextElement();
+                if (entry.getName().toLowerCase().contains(pattern)
+                        || entry.getName().toLowerCase().contains(pattern.replace(".", "/"))
+                        ) {
+                    result.add(jar.getName());
+                }
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+
     }
 }
--- src/test/java/org/findclass/FindClassTest.java
+++ src/test/java/org/findclass/FindClassTest.java
@@ -1,32 +1,18 @@
 package org.findclass;
 
-import org.junit.Before;
-
 import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
+
+import static junit.framework.TestCase.assertNotNull;
 
 public class FindClassTest {
 
-    private Collection<File> result;
 
-    @Before
-    public void setup() throws IOException {
-        final File dir = createDirectory("testdir");
-        result=ClassFinder.searchIn(dir).find();
-    }
-
-    private File createDirectory(final String testdir) throws IOException {
-        final File dir = new File(testdir);
-        dir.mkdirs();
-        final File classFile = new File(dir, "ThisClassShouldBefound.clas");
-        classFile.createNewFile();
-        Runtime.getRuntime().exec("jar cvf  test.jar *.class");
-        return dir;
-    }
+    final String testdir = "src/test/resources";
 
     @org.junit.Test
     public void testFindClassInJar() throws Exception {
-
+        Collection<String> files = ClassFinder.searchIn(new File(testdir)).find("test");
+        assertNotNull(files);
     }
 }
